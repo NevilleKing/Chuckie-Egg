72e343f5809aaf7b33c3f47730d68c380f052dea
shearer12345 <shearer12345@gmail.com>
2016-02-08 10:34:23 +0000

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+-- A solution contains projects, and defines the available configurations
+solution "sdl2template"
+	configurations { "Debug", "Release"}
+
+	flags { "Unicode" , "NoPCH"}
+
+	projectName = "sdl2template"
+	sourcePath = "src"
+	binPath = "bin"
+
+	-- A project defines one build target
+	project (projectName)
+		kind "ConsoleApp"
+		language "C++"
+		targetdir ( binPath )
+
+		configuration { "windows" }
+			buildoptions ""
+			linkoptions { "/NODEFAULTLIB:msvcrt" } -- https://github.com/yuriks/robotic/blob/master/premake5.lua
+		configuration { "linux" }
+			buildoptions "-std=c++11" --http://industriousone.com/topic/xcode4-c11-build-option
+			toolset "gcc"
+		configuration {}
+
+		files { path.join(sourcePath, "**.h"), path.join(sourcePath, "**.cpp") } -- build all .h and .cpp files recursively
+		excludes { "./graphics_dependencies/**" }  -- don't build files in graphics_dependencies/
+
+
+		-- where are header files?
+		configuration "windows"
+		includedirs {
+							"./graphics_dependencies/SDL2/include",
+							"./graphics_dependencies/SDL2/include/SDL2",
+							"./graphics_dependencies/glew/include",
+							"./graphics_dependencies/glm",
+							"./graphics_dependencies/SDL2_image/include",
+							"./graphics_dependencies/SDL2_ttf/include",
+							"./graphics_dependencies/SDL2_mixer/include",
+						 }
+		configuration { "linux" }
+		includedirs {
+					 -- should be installed as in ./graphics_dependencies/README.asciidoc
+						 }
+		configuration {}
+
+
+		-- what libraries need linking to
+		configuration "windows"
+			links { "SDL2", "SDL2main", "opengl32", "glew32", "SDL2_image", "SDL2_ttf", "SDL2_mixer" }
+		configuration "linux"
+			links { "SDL2", "SDL2main", "GL", "GLEW", "SDL2_image", "SDL2_ttf", "SDL2_mixer" }
+		configuration {}
+
+		-- where are libraries?
+		configuration "windows"
+		libdirs {
+					"./graphics_dependencies/glew/lib/Release/Win32",
+					"./graphics_dependencies/SDL2/lib/win32",
+					"./graphics_dependencies/SDL2_image/lib/x86/",
+					"./graphics_dependencies/SDL2_ttf/lib/x86/",
+					"./graphics_dependencies/SDL2_mixer/lib/x86/",
+					}
+		configuration "linux"
+					 -- should be installed as in ./graphics_dependencies/README.asciidoc
+		configuration {}
+
+
+		configuration "*Debug"
+			defines { "DEBUG" }
+			flags { "Symbols" }
+			optimize "Off"
+			targetsuffix "-debug"
+
+
+		configuration "*Release"
+			defines { "NDEBUG" }
+			optimize "On"
+			targetsuffix "-release"
+
+
+		-- copy dlls on windows
+		if os.get() == "windows" then
+			-- SDL2
+			os.copyfile("./graphics_dependencies/SDL2/lib/win32/SDL2.dll", path.join(binPath, "SDL2.dll"))
+
+			-- SDL2_image
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/SDL2_image.dll", path.join(binPath, "SDL2_image.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/libjpeg-9.dll", path.join(binPath, "libjpeg-9.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/libpng16-16.dll", path.join(binPath, "libpng16-16.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/libtiff-5.dll", path.join(binPath, "libtiff-5.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/libwebp-4.dll", path.join(binPath, "libwebp-4.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_image/lib/x86/zlib1.dll", path.join(binPath, "zlib1.dll"))
+
+			-- SDL2_ttf
+			os.copyfile("./graphics_dependencies/SDL2_ttf/lib/x86/SDL2_ttf.dll", path.join(binPath, "SDL2_ttf.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_ttf/lib/x86/libfreetype-6.dll", path.join(binPath, "libfreetype-6.dll"))
+				-- also requires zlib1, but copied already
+
+			-- SDL2_mixer
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/SDL2_mixer.dll", path.join(binPath, "SDL2_mixer.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/libFLAC-8.dll", path.join(binPath, "libFLAC-8.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/libmodplug-1.dll", path.join(binPath, "libmodplug-1.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/libFLAC.dll", path.join(binPath, "libFLAC.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/libvorbis-0-0.dll", path.join(binPath, "libvorbis-0-0.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/libvorbisfile-3.dll", path.join(binPath, "libvorbisfile-3.dll"))
+			os.copyfile("./graphics_dependencies/SDL2_mixer/lib/x86/smpeg2.dll", path.join(binPath, "smpeg2.dll"))
+
+			-- glew
+			os.copyfile("./graphics_dependencies/glew/bin/Release/Win32/glew32.dll", path.join(binPath, "glew32.dll"))
+		end
---
+++ src/main.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+
+#ifdef _WIN32 // compiling on windows
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#else // NOT compiling on windows
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#endif
+
+std::string exeName;
+SDL_Window *win; //pointer to the SDL_Window
+SDL_Renderer *ren; //pointer to the SDL_Renderer
+SDL_Surface *surface; //pointer to the SDL_Surface
+SDL_Texture *tex; //pointer to the SDL_Texture
+
+bool done = false;
+
+void handleInput()
+{
+	//Event-based input handling
+	//The underlying OS is event-based, so **each** key-up or key-down (for example)
+	//generates an event.
+	//  - https://wiki.libsdl.org/SDL_PollEvent
+	//In some scenarios we want to catch **ALL** the events, not just to present state
+	//  - for instance, if taking keyboard input the user might key-down two keys during a frame
+	//    - we want to catch based, and know the order
+	//  - or the user might key-down and key-up the same within a frame, and we still want something to happen (e.g. jump)
+	//  - the alternative is to Poll the current state with SDL_GetKeyboardState
+
+	SDL_Event event; //somewhere to store an event
+
+	//NOTE: there may be multiple events per frame
+	while (SDL_PollEvent(&event)) //loop until SDL_PollEvent returns 0 (meaning no more events)
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			done = true; //set donecreate remote branch flag if SDL wants to quit (i.e. if the OS has triggered a close event,
+							//  - such as window close, or SIGINT
+			break;
+
+			//keydown handling - we should to the opposite on key-up for direction controls (generally)
+		case SDL_KEYDOWN:
+			//Keydown can fire repeatable if key-repeat is on.
+			//  - the repeat flag is set on the keyboard event, if this is a repeat event
+			//  - in our case, we're going to ignore repeat events
+			//  - https://wiki.libsdl.org/SDL_KeyboardEvent
+			if (!event.key.repeat)
+				switch (event.key.keysym.sym)
+				{
+					//hit escape to exit
+					case SDLK_ESCAPE: done = true;
+				}
+			break;
+		}
+	}
+}
+// end::handleInput[]
+
+// tag::updateSimulation[]
+void updateSimulation(double simLength = 0.02) //update simulation with an amount of time to simulate for (in seconds)
+{
+  //CHANGE ME
+}
+
+void render()
+{
+		//First clear the renderer
+		SDL_RenderClear(ren);
+		//Draw the texture
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		//Update the screen
+		SDL_RenderPresent(ren);
+}
+
+// based on http://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world/
+int main( int argc, char* args[] )
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		exit(1);
+	}
+	std::cout << "SDL initialised OK!\n";
+
+	//create window
+	win = SDL_CreateWindow("SDL Hello World!", 100, 100, 600, 600, SDL_WINDOW_SHOWN);
+
+	//error handling
+	if (win == nullptr)
+	{
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		exit(1);
+	}
+	std::cout << "SDL CreatedWindow OK!\n";
+
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr)
+	{
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	std::string imagePath = "../assets/Opengl-logo.svg.png";
+	surface = IMG_Load(imagePath.c_str());
+	if (surface == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL IMG_Load Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	tex = SDL_CreateTextureFromSurface(ren, surface);
+	SDL_FreeSurface(surface);
+	if (tex == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	while (!done) //loop until done flag is set)
+	{
+		handleInput(); // this should ONLY SET VARIABLES
+
+		updateSimulation(); // this should ONLY SET VARIABLES according to simulation
+
+		render(); // this should render the world state according to VARIABLES
+
+		SDL_Delay(20); // unless vsync is on??
+	}
+
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	return 0;
+}
