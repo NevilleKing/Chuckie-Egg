bf1e0b7135d4c4b0f4d0d481f9d5fbba1bd9b8e1
Neville King <nevster07@gmail.com>
2016-03-07 14:23:24 +0000

    Added audio include

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #else // NOT compiling on windows
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 #endif
 
 // custom classes
